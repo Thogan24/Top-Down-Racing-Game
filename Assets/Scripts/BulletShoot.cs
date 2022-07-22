7e4c80e9765520f3632ba26fdcd417ebc4006f63
Thogan24 <thogan1924@gmail.com>
2022-07-22 18:50:07 -0400

    Recoil (didn't work)

--- Assets/Scripts/BulletShoot.cs
+++ Assets/Scripts/BulletShoot.cs
@@ -10,16 +10,46 @@ public class BulletShoot : MonoBehaviour
     public float force;
     public GameObject player;
     public Vector2 vector2;
-
-
+    public Vector2 shootPosition;
+    public float recoilForce;
+    public Rigidbody2D playerRB;
+    public float time = 0;
+    public bool recoil = false;
     
+    private void Start()
+    {
+        playerRB = player.GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         
         if (Input.GetMouseButtonDown(0))
         {
+            
             Shoot();
+            
         }
+        if (recoil)
+        {
+            time += Time.deltaTime;
+
+            if (time < 1)
+            {
+                player.GetComponent<Rigidbody2D>().AddForce(shootPosition * -recoilForce);
+
+            }
+            else
+            {
+                time = 0;
+                recoil = false;
+            }
+        }
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    player.GetComponent<Rigidbody2D>().AddForceAtPosition(firePoint.up * -recoilForce, player.transform.position);
+        //}
     }
 
     void Shoot()
@@ -28,11 +58,14 @@ public class BulletShoot : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
         
-        vector2.x *= firePoint.position.x;
-        vector2.y *= firePoint.position.y;
+        //vector2.x *= firePoint.position.x;
+        //vector2.y *= firePoint.position.y;
         Debug.Log(vector2);
-        player.GetComponent<Rigidbody2D>().AddForce(vector2 * bulletForce);
 
+        shootPosition = firePoint.up;
+        //player.GetComponent<Rigidbody2D>().MovePosition(playerRB.position * -recoilForce);
         vector2 = Vector2.one;
+        recoil = true;
+        time = 0;
     }
 }
