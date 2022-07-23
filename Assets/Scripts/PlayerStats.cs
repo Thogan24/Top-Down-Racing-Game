97af5518929fbfbf4f38650dd47cf6eb9a46424e
Thogan24 <thogan1924@gmail.com>
2022-07-23 12:24:13 -0400

    Made backtick turn on stats

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,12 +5,14 @@ using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
+    public bool showStats;
+
+    public GameObject statsCanvas;
     private Movement MovementScript;
     private BulletShoot BulletShootScript;
     public GameObject player;
     public TextMeshProUGUI StatsTextbox;
     
-
     void Start() {
         MovementScript = player.GetComponent<Movement>();
         BulletShootScript = player.GetComponent<BulletShoot>();
@@ -18,6 +20,21 @@ public class PlayerStats : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.BackQuote) == true)
+        {
+
+            
+            if (showStats == false)
+            {
+                statsCanvas.SetActive(true);
+                showStats = true;
+            }
+            else
+            {
+                statsCanvas.SetActive(false);
+                showStats = false;
+            }
+        }
         StatsTextbox.text = "Speed: " + MovementScript.speedTracker.ToString("F2") +
         "<br>MovementSpeed: " + MovementScript.movementSpeed.ToString() +
         "<br>Angle: " + MovementScript.angle.ToString("F2") +
