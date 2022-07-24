6e5dbccb6852bf58b098d609683a3601d6da7252
Thogan24 <thogan1924@gmail.com>
2022-07-23 20:57:10 -0400

    Made RGB minimap

--- Assets/Scripts/MinimapScript.cs
+++ Assets/Scripts/MinimapScript.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinimapScript : MonoBehaviour
 {
 
     public Transform player;
+    public GameObject miniMapBorder;
+    float h;
+    float s;
+    float v;
 
-    private void LateUpdate()
+    private void Update()
     {
-        Vector2 newPosition = player.position;
-        newPosition.y = transform.position.y;
-        transform.position = newPosition;
+        transform.position = new Vector3(player.position.x, player.position.y, -15);
+        
+
+        Color.RGBToHSV(miniMapBorder.GetComponent<Image>().color, out h, out s, out v);
+        miniMapBorder.GetComponent<Image>().color = Color.HSVToRGB(h + Time.deltaTime * .25f, s, v);
+        Debug.Log(h);
+
     }
 }
+ 
