0401cb399be8ffbf8aa68913ede5006ded08826e
Thogan24 <thogan1924@gmail.com>
2022-07-29 22:38:10 -0400

    Made timer go away if you press t

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,12 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI TimerTextbox;
+    public GameObject TimerObject;
     public float timer;
     public bool startTimer;
+    public bool showTimer;
     public GameObject finishLine;
+    //public TextMeshProUGUI Timer;
 
     void Update()
     {
@@ -16,6 +19,20 @@ public class GameManager : MonoBehaviour
             TimerTextbox.text = "Time: " + timer.ToString("F3");
         }
 
+        if (Input.GetKeyDown(KeyCode.T) == true)
+        {
 
+
+            if (showTimer == false)
+            {               
+                showTimer = true;
+                TimerObject.SetActive(true);
+            }
+            else
+            {
+                showTimer = false;
+                TimerObject.SetActive(false);
+            }
+        }
     }
 }
