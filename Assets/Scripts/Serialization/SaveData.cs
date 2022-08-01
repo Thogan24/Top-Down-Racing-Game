03f59bd88ba7ad3af4127e5a05145ae1e037e244
Thogan24 <thogan1924@gmail.com>
2022-08-01 12:43:14 -0400

    Save System Implemented (doesn't work)

---
+++ Assets/Scripts/Serialization/SaveData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    private static SaveData _current;
+    public static SaveData current
+    {
+        get {
+            if(_current == null)
+            {
+                _current = new SaveData();
+            }
+            return _current; 
+        }
+    }
+
+    public PlayerProfile profile;
+    public string name;
+}
